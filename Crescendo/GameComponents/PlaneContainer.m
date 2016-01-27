a9951c036565f2eb21a3eacb5a702eeb3f291f0a
Administrator <admin@btgames-mac-mini-02.local>
2016-01-26 23:11:30 -0800

    Created my first methods!

--- Crescendo/GameComponents/PlaneContainer.m
+++ Crescendo/GameComponents/PlaneContainer.m
@@ -10,4 +10,22 @@
 
 @implementation PlaneContainer
 
+- (id)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        self.planes = [[NSMutableArray alloc] init];
+    }
+    
+    return self;
+}
+
+-(void)update
+
+{
+    
+}
+
 @end
