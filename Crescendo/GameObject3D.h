cbdcc689327806f68f73ad31881d4ec52bfccf75
Sean Wang <swang182@my.bcit.ca>
2016-02-12 13:23:33 -0800

    Added some game object 3d support methods.

--- Crescendo/GameObject3D.h
+++ Crescendo/GameObject3D.h
@@ -11,9 +11,18 @@
 
 @interface GameObject3D : NSObject
 {
-    @public GLfloat vertices[216];
-    @public GLKVector3 worldPosition;
+@public GLfloat vertices[216];
+@public GLKVector3 worldPosition;
+@public GLKVector3 rotation;
+@public GLKVector3 scale;
 }
 
+-(GLKVector3)GetUp;
+-(GLKVector3)GetRight;
+-(GLKVector3)GetFoward;
+
+-(GLKMatrix4)GetTranslationMatrix;
+-(GLKMatrix4)GetRotationMatrix;
+-(GLKMatrix4)GetScaleMatrix;
 
 @end
