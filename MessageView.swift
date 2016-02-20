8c66d43b429ca88c0c377f289ccb414821faee55
matthewlouis <matthew.moldowan@gmail.com>
2016-02-20 15:08:48 -0800

    Implemented Message System, with animations and Title Screen

---
+++ MessageView.swift
@@ -0,0 +1,21 @@
+//
+//  MessageView.swift
+//  Crescendo
+//
+//  Created by Matthew Moldowan on 2016-02-20.
+//  Copyright © 2016 Equalizer. All rights reserved.
+//
+
+import UIKit
+
+class MessageView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
